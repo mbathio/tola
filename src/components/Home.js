6cd648b0b2ec9d8ef04e4dc1d857568c5a403359
mbathio <mbathiobadiallo20@gmail.com>
2024-07-17 03:03:38 +0200

    Fixed module resolution issues in App.js and Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { db } from './firebase';
+import { db, auth } from '../firebase/firebase';
+
 
 const Home = () => {
   const [questions, setQuestions] = useState([]);
